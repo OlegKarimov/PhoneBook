1f98990822d4ddb8f9d96a3c064830bba3e7777b
Oleg <126770172+olegkarimov@users.noreply.github.com>
2023-06-08 12:25:25 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 # PhoneBook
 Selenium-java tests for 'Phone Book' website
+=======
+# QA23_PhoneBook
+Selenium-java tests for 'PhoneBook' website
+>>>>>>> 2f713c9 (Initial commit)
---
+++ src/test/java/com/phonebook/tests/CreateAccountTests.java
@@ -0,0 +1,31 @@
+package com.phonebook.tests;
+
+import com.phonebook.model.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CreateAccountTests extends TestBase {
+
+    //precondition: user should be logged out
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (!app.getHeader().isLoginLinkPresent()) {
+            app.getHeader().clickOnSignOutButton();
+        }
+        //click on Login link
+        app.getHeader().clickOnLoginLink();
+    }
+
+    @Test
+    public void existedUserRegistrationNegativeTest() {
+        //enter email field
+        //enter password field
+        app.getUser().fillLoginRegistrationForm(new User().setEmail("kan@gmai.com").setPassword("Kan123$-_$"));
+        //click on Registration
+        app.getUser().clickOnRegistrationButton();
+        //assert user logged in(check Sign Out button displayed)
+        Assert.assertTrue(app.getUser().isAlertPresent());
+    }
+
+}
